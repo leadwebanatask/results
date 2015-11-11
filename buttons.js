e2c54c3aeaf21d4663234a33d4abf8db1eda8558
leadwebanatask <fabryka@n30.info>
2015-11-11 12:16:02 +0100

    buttons.js  code formatting

--- buttons.js
+++ buttons.js
@@ -1,8 +1,10 @@
 for (var i = 0; i < 5; i++) {
-(function (num) {
-  var btn = document.createElement('button');
-  btn.appendChild(document.createTextNode('Button ' + num));
-  btn.addEventListener('click', function(){ console.log(num); });
-  document.body.appendChild(btn);
-})(i);
+	(function(num) {
+		var btn = document.createElement('button');
+		btn.appendChild(document.createTextNode('Button ' + num));
+		btn.addEventListener('click', function() {
+			console.log(num);
+		});
+		document.body.appendChild(btn);
+	})(i);
 }
